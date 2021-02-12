6c331e436e9d678539a4122c1f9531f97e638133
Maybees <8634709+Maybees@users.noreply.github.com>
2021-02-11 19:30:20 -0800

    feat: Adding basic policy run function

--- YamCheck.psm1
+++ YamCheck.psm1
@@ -1,2 +1,55 @@
 #Requires -Modules @{ ModuleName="powershell-yaml"; ModuleVersion="0.4.2" }
 
+function Assert-YamlPolicies {
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory = $true)] [System.IO.FileInfo[]] $YamlDirectories,
+    [Parameter(Mandatory = $true)] [System.IO.FileInfo[]] $PolicyDirectories
+  )
+
+  $yamls = Get-YamlFiles $YamlDirectories
+
+  $policies = $PolicyDirectories | ForEach-Object {
+    $policyDir = $_
+    Get-ChildItem $policyDir -Include *.psm1 -Recurse | ForEach-Object { 
+      $filePolicies = (Import-Module $_.FullName -PassThru).ExportedFunctions.Values | ForEach-Object {
+        $checkFunc = $_
+        [PSCustomObject]@{
+          PolicyScript  = $_
+          Results       = $yamls | ForEach-Object { 
+            $yaml = $_
+            $checkSplat = @{
+              ScriptBlock   = $checkFunc.ScriptBlock
+              ArgumentList  = $yaml.Yaml
+            }
+            [PSCustomObject]@{
+              File = $yaml.Filepath
+              Result = Invoke-Command @checkSplat
+            }
+           }
+        }
+      }
+      [PSCustomObject]@{
+        PolicyFile = $_.BaseName
+        Policies = $filePolicies
+      }
+    }
+  }
+
+  $policies
+}
+
+function Get-YamlFiles {
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory = $true)] [System.IO.FileInfo[]] $YamlDirectories
+  )
+  $YamlDirectories | ForEach-Object {
+    Get-ChildItem $_ -Include *.yml -Recurse | ForEach-Object {
+      @{
+        Filepath  = $_.BaseName
+        Yaml      = Get-Content $_ | ConvertFrom-Yaml
+      }
+    }
+  }
+}
