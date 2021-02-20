28112af683e259aee3b6fe8418f6c3bbda0b2526
Erik McPherson <ErikMcPherson@UmpquaBank.com>
2021-02-19 18:55:32 -0800

    test: Updating tests to fit new function structure

--- tests/YamCheck.Tests.ps1
+++ tests/YamCheck.Tests.ps1
@@ -1,48 +1,52 @@
-Describe 'Assert-YamlPolicies' {
+Describe 'Get-PolicyResults' {
   BeforeAll {
     Get-Module 'YamCheck' | Remove-Module
     Get-Module 'powershell-yaml' | Remove-Module
     Install-Module -Name 'powershell-yaml' -RequiredVersion '0.4.2' -Force -AllowClobber
     Import-Module 'powershell-yaml'
-    Import-Module $PSScriptRoot/../YamCheck.psd1
+    Import-Module $PSScriptRoot/../YamCheck.psm1 # Importing psm1 for access to private functions
   }
 
   It 'Given a yaml template without a property required by policy, returns a failed check' {
-    $policyResults = Assert-YamlPolicies -PolicyDirectories $PSScriptRoot/test-policies -YamlDirectories $PSScriptRoot/test-yaml
-    $testResult = $policyResults.Policies |
-      Where-Object { $_.PolicyScript.Name -eq 'Assert-RequiredPropertyExists' } |
-      Select-Object -Expand Results |
-      Where-Object File -eq 'bad-template' |
+    $policyResults = Get-PolicyResults -PolicyDirectories $PSScriptRoot/test-policies -YamlDirectories $PSScriptRoot/test-yaml
+    $testResult = $policyResults |
+      Where-Object { 
+        $_.Policy.PolicyName -eq 'Assert-RequiredPropertyExists' -and
+        $_.YamlFile.YamlFileName -eq 'bad-template'
+      } |
       Select-Object -Expand Result
     $testResult | Should -Be $false
   }
 
   It 'Given a yaml template with a property required by policy, returns a successful check' {
-    $policyResults = Assert-YamlPolicies -PolicyDirectories $PSScriptRoot/test-policies -YamlDirectories $PSScriptRoot/test-yaml
-    $testResult = $policyResults.Policies |
-      Where-Object { $_.PolicyScript.Name -eq 'Assert-RequiredPropertyExists' } |
-      Select-Object -Expand Results |
-      Where-Object File -eq 'good-template' |
+    $policyResults = Get-PolicyResults -PolicyDirectories $PSScriptRoot/test-policies -YamlDirectories $PSScriptRoot/test-yaml
+    $testResult = $policyResults |
+      Where-Object { 
+        $_.Policy.PolicyName -eq 'Assert-RequiredPropertyExists' -and
+        $_.YamlFile.YamlFileName -eq 'good-template'
+      } |
       Select-Object -Expand Result
     $testResult | Should -Be $true
   }
 
   It 'Given a yaml template with a property not matching a value required by policy, returns a failed check' {
-    $policyResults = Assert-YamlPolicies -PolicyDirectories $PSScriptRoot/test-policies -YamlDirectories $PSScriptRoot/test-yaml
-    $testResult = $policyResults.Policies |
-      Where-Object { $_.PolicyScript.Name -eq 'Assert-SubPropertyEqualsValue' } |
-      Select-Object -Expand Results |
-      Where-Object File -eq 'bad-template' |
+    $policyResults = Get-PolicyResults -PolicyDirectories $PSScriptRoot/test-policies -YamlDirectories $PSScriptRoot/test-yaml
+    $testResult = $policyResults |
+      Where-Object { 
+        $_.Policy.PolicyName -eq 'Assert-SubPropertyEqualsValue' -and
+        $_.YamlFile.YamlFileName -eq 'bad-template'
+      } |
       Select-Object -Expand Result
     $testResult | Should -Be $false
   }
 
   It 'Given a yaml template with a property matching a value required by policy, returns a successful check' {
-    $policyResults = Assert-YamlPolicies -PolicyDirectories $PSScriptRoot/test-policies -YamlDirectories $PSScriptRoot/test-yaml
-    $testResult = $policyResults.Policies |
-      Where-Object { $_.PolicyScript.Name -eq 'Assert-SubPropertyEqualsValue' } |
-      Select-Object -Expand Results |
-      Where-Object File -eq 'good-template' |
+    $policyResults = Get-PolicyResults -PolicyDirectories $PSScriptRoot/test-policies -YamlDirectories $PSScriptRoot/test-yaml
+    $testResult = $policyResults |
+      Where-Object { 
+        $_.Policy.PolicyName -eq 'Assert-SubPropertyEqualsValue' -and
+        $_.YamlFile.YamlFileName -eq 'good-template'
+      } |
       Select-Object -Expand Result
     $testResult | Should -Be $true
   }
